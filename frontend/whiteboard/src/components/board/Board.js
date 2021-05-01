481cb4a40caf52a974755fe3eb6ed48e75530426
Pawel Marcinkiewicz <paweljmarcinkiewicz@gmail.com>
2021-05-01 23:30:37 +0100

    changed to heroku url in socket io constant in board.js

--- frontend/whiteboard/src/components/board/Board.js
+++ frontend/whiteboard/src/components/board/Board.js
@@ -4,7 +4,7 @@ import io from "socket.io-client";
 
 import "./style.css";
 
-const socket = io(window.location.hostname);
+const socket = io("https://collaborative-whiteboard.herokuapp.com/");
 
 const Board = () => {
   const [mouseDown, setMouseDown] = useState(false);
